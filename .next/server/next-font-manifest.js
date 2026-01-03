c7f90aef122c0fba5ee29c59b3b58fed29d1b4e9
shahed106 <shahed106@example.com>
2026-01-03 16:06:17 +0600

    Fix: Remove .next build files from git and update .gitignore

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/skevan/Documents/Codes 101/NextJS/Dev_AmarVote/Dev_3.1.26 AmarVote/AmarVote/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
